b2c59ce7966702aac4bb7feee5f94496222296fa
SantinoQuevedo <santino.quevedo@mi.unc.edu.ar>
2025-11-03 21:57:25 -0300

    cambios para mañana

--- src/database/services/services_events.py
+++ src/database/services/services_events.py
@@ -4,32 +4,44 @@ from sqlalchemy import desc, func
 from src.database.database import SessionLocal, get_db
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
-from src.database.models import Player, Card , Detective , Event, Secrets, Game, Set
+from src.database.models import Player, Card, Detective, Event, Secrets, Game, Set
 from src.database.services.services_games import finish_game
 from src.database.services.services_secrets import steal_secret as steal_secret_service
-from typing import List 
+from typing import List
+
 
 def cards_off_table(player_id: int, db: Session):
     """
     descarta las cartas not so fast de un jugador
     """
-    nsf = db.query(Event).filter(Event.name == "Not so fast", Event.player_id == player_id, Event.dropped == False).all()
+    nsf = (
+        db.query(Event)
+        .filter(
+            Event.name == "Not so fast",
+            Event.player_id == player_id,
+            Event.dropped == False,
+        )
+        .all()
+    )
 
     if not nsf:
         # No hay cartas "Not so fast" para este jugador, no hay nada que hacer
         return {"message": "No 'Not so fast' cards found for this player to discard."}
     try:
         for event in nsf:
-            event.dropped = True        
-        db.commit() # se descartan las cartas nsf del jugador
+            event.dropped = True
+        db.commit()  # se descartan las cartas nsf del jugador
     except Exception as e:
-        db.rollback() 
-        raise HTTPException(status_code=400, detail=f"Error discarding 'Not so fast' cards: {str(e)}")
+        db.rollback()
+        raise HTTPException(
+            status_code=400, detail=f"Error discarding 'Not so fast' cards: {str(e)}"
+        )
+
 
 def look_into_ashes(player_id: int, card_id: int, db: Session):
     """
-    mira las ultimas 5 cartas de la pila de descarte y toma una 
-    en realidad le llega una card_id del front que. esta la funcion que le muestra las 5 
+    mira las ultimas 5 cartas de la pila de descarte y toma una
+    en realidad le llega una card_id del front que. esta la funcion que le muestra las 5
     cartas del descarte. entonces en el endpoint que llama esta funcion solo elije una de esas 5 cartas
     y le cambio dueno y dropped por true
     """
@@ -39,18 +51,21 @@ def look_into_ashes(player_id: int, card_id: int, db: Session):
     try:
         card.dropped = False
         card.player_id = player_id
-        card.discardInt = 0 #la carta vuelve a estar en juego
-        card.picked_up=True
+        card.discardInt = 0  # la carta vuelve a estar en juego
+        card.picked_up = True
         db.commit()
         db.refresh(card)
-        return card 
+        return card
     except Exception as e:
         db.rollback()
-        raise HTTPException(status_code=400, detail=f"Error assigning card to player: {str(e)}")
+        raise HTTPException(
+            status_code=400, detail=f"Error assigning card to player: {str(e)}"
+        )
+
 
 def one_more(receive_secret_player_id: int, secret_id: int, db: Session):
     """
-    Choose one revealed secret card and add it, face-down, to any player's secrets, 
+    Choose one revealed secret card and add it, face-down, to any player's secrets,
     including your own. This may remove social disgrace.
     """
     try:
@@ -61,7 +76,9 @@ def one_more(receive_secret_player_id: int, secret_id: int, db: Session):
         raise e
     except Exception as e:
         db.rollback()
-        raise HTTPException(status_code=400, detail=f"Error executing 'One More' event: {str(e)}")
+        raise HTTPException(
+            status_code=400, detail=f"Error executing 'One More' event: {str(e)}"
+        )
 
 
 def delay_the_murderers_escape(game_id: int, db: Session):
@@ -72,57 +89,69 @@ def delay_the_murderers_escape(game_id: int, db: Session):
     """
 
     game = db.query(Game).filter(Game.game_id == game_id).first()
-    if not game : 
+    if not game:
         raise HTTPException(status_code=404, detail="No game found ")
-    
 
-    # Agarrro maximo las ultimas 5 cartas descartadas 
-    last_discarded_cards = db.query(Card).filter(
-        Card.game_id == game_id,
-        Card.dropped == True
-    ).order_by(desc(Card.discardInt)).limit(5).all()
+    # Agarrro maximo las ultimas 5 cartas descartadas
+    last_discarded_cards = (
+        db.query(Card)
+        .filter(Card.game_id == game_id, Card.dropped == True)
+        .order_by(desc(Card.discardInt))
+        .limit(5)
+        .all()
+    )
 
     if not last_discarded_cards:
-        raise HTTPException(status_code=404, detail="No cards found in the discard pile for this game.")
-    for card in last_discarded_cards : 
-        card.dropped = False 
-        card.picked_up = False 
+        raise HTTPException(
+            status_code=404, detail="No cards found in the discard pile for this game."
+        )
+    for card in last_discarded_cards:
+        card.dropped = False
+        card.picked_up = False
         card.draft = False
-        card.player_id = None  
-        card.discardInt = -1 
-        game.cards_left += 1 
-    try : 
+        card.player_id = None
+        card.discardInt = -1
+        game.cards_left += 1
+    try:
         db.commit()
     except Exception as e:
         db.rollback()
-        raise HTTPException(status_code=400, detail=f"Error executing 'Delay murderer escapes' event: {str(e)}")
-    
+        raise HTTPException(
+            status_code=400,
+            detail=f"Error executing 'Delay murderer escapes' event: {str(e)}",
+        )
 
     return last_discarded_cards
-    
 
 
 def early_train_paddington(game_id: int, db: Session):
     """
     Implement the effect of the 'Early Train to Paddington' event.
     """
-    deck = db.query(Card).filter(Card.game_id == game_id, Card.player_id == None, Card.draft == False).all()
-    
+    deck = (
+        db.query(Card)
+        .filter(Card.game_id == game_id, Card.player_id == None, Card.draft == False)
+        .all()
+    )
+
     if not deck:
         raise HTTPException(status_code=404, detail="Deck not found.")
-    
-    if len(deck)<6:
-        finish_game(game_id) # se termina el juego si no hay mas cartas en el mazo
+
+    if len(deck) < 6:
+        finish_game(game_id)  # se termina el juego si no hay mas cartas en el mazo
         return {"message": "Not enough cards in the deck. The game has ended."}
-    
+
     random.shuffle(deck)
-    max_discardInt = db.query(func.max(Card.discardInt)).filter(Card.game_id == game_id).scalar() or 0
+    max_discardInt = (
+        db.query(func.max(Card.discardInt)).filter(Card.game_id == game_id).scalar()
+        or 0
+    )
     cards_to_discard = deck[:6]
     for card in cards_to_discard:
         card.dropped = True
         card.picked_up = False
         max_discardInt += 1
-        card.discardInt = max_discardInt # Asigna el siguiente valor en la secuencia
+        card.discardInt = max_discardInt  # Asigna el siguiente valor en la secuencia
     try:
         db.commit()
         for card in cards_to_discard:
@@ -130,28 +159,38 @@ def early_train_paddington(game_id: int, db: Session):
         return {"message": "Early Train to Paddington event executed successfully."}
     except Exception as e:
         db.rollback()
-        raise HTTPException(status_code=400, detail=f"Error executing 'Early Train to Paddington' event: {str(e)}")
+        raise HTTPException(
+            status_code=400,
+            detail=f"Error executing 'Early Train to Paddington' event: {str(e)}",
+        )
 
-def point_your_suspicion (game_id : int ,db : Session = Depends (get_db)) : 
+
+def point_your_suspicion(game_id: int, db: Session = Depends(get_db)):
     game = db.query(Game).filter(Game.game_id == game_id).first()
-    if not game : 
+    if not game:
         raise HTTPException(status_code=404, detail="Game not found.")
-    game.status = "Voting"
-    try : 
+    try:
         db.commit()
         return {"message": "Point your Suspicion event executed successfully."}
     except Exception as e:
         db.rollback()
-        raise HTTPException(status_code=400, detail=f"Error executing 'Point Your Suspicion' event: {str(e)}")
-    
-def end_point_your_suspicion(game_id : int, db :Session = Depends (get_db)) : 
+        raise HTTPException(
+            status_code=400,
+            detail=f"Error executing 'Point Your Suspicion' event: {str(e)}",
+        )
+
+
+def end_point_your_suspicion(game_id: int, db: Session = Depends(get_db)):
     game = db.query(Game).filter(Game.game_id == game_id).first()
-    if not game : 
+    if not game:
         raise HTTPException(status_code=404, detail="Game not found.")
     game.status = "in course"
-    try : 
+    try:
         db.commit()
         return {"message": "Point your Suspicion ending event executed successfully."}
     except Exception as e:
         db.rollback()
-        raise HTTPException(status_code=400, detail=f"Error executing 'Point Your Suspicion' ending event: {str(e)}")
+        raise HTTPException(
+            status_code=400,
+            detail=f"Error executing 'Point Your Suspicion' ending event: {str(e)}",
+        )
--- src/routes/players_routes.py
+++ src/routes/players_routes.py
@@ -1,32 +1,48 @@
-from fastapi import APIRouter, Depends, HTTPException  #te permite definir las rutas o subrutas por separado
-from sqlalchemy.orm import Session  
-from src.database.services.services_websockets import broadcast_game_information, broadcast_player_state
+from fastapi import (
+    APIRouter,
+    Depends,
+    HTTPException,
+)  # te permite definir las rutas o subrutas por separado
+from sqlalchemy.orm import Session
+from src.database.services.services_websockets import (
+    broadcast_game_information,
+    broadcast_player_state,
+)
 from src.database.database import SessionLocal, get_db
-from src.database.models import Game, Player 
+from src.database.models import Game, Player
 from src.schemas.players_schemas import Player_Base
 from src.database.services.services_games import update_players_on_game
+from sqlalchemy import desc, func
 
-player = APIRouter() # ahora el player es lo mismo que hacer app
+player = APIRouter()  # ahora el player es lo mismo que hacer app
 
-#FALTA MANEJO  DE ERRORES por ids inexistentes de parametros, sobre todo que no haya internal server error (sucede al crear un player con un game id inexistente)
-#Luego list players cuando le paso un game id inexistente no debe responder 200 sino que 404 NOT FOUND
+# FALTA MANEJO  DE ERRORES por ids inexistentes de parametros, sobre todo que no haya internal server error (sucede al crear un player con un game id inexistente)
+# Luego list players cuando le paso un game id inexistente no debe responder 200 sino que 404 NOT FOUND
 
-#En general falta la estructura de si salta exception agarrarla y devolver el codigo correspondientev
+# En general falta la estructura de si salta exception agarrarla y devolver el codigo correspondientev
 
-@player.get ("/lobby/players/{game_id}", tags = ["Players"])
-def list_players(game_id : int ,db: Session = Depends(get_db)):
-    players = db.query(Player).filter(Player.game_id == game_id).all() # .all() me devuelve una lista, si no hay nada devuelve lista vacia
+
+@player.get("/lobby/players/{game_id}", tags=["Players"])
+def list_players(game_id: int, db: Session = Depends(get_db)):
+    players = (
+        db.query(Player).filter(Player.game_id == game_id).all()
+    )  # .all() me devuelve una lista, si no hay nada devuelve lista vacia
     if not players:
-        raise HTTPException(status_code=404, detail="game not found or no players in this game")
+        raise HTTPException(
+            status_code=404, detail="game not found or no players in this game"
+        )
     return players
 
-@player.post("/players", status_code=201, tags = ["Players"])
-def create_player(player : Player_Base, db: Session = Depends(get_db)):
-    new_player = Player (name = player.name,
-                            host = player.host,
-                            game_id = player.game_id,
-                            birth_date = player.birth_date,
-                            avatar = player.avatar)
+
+@player.post("/players", status_code=201, tags=["Players"])
+def create_player(player: Player_Base, db: Session = Depends(get_db)):
+    new_player = Player(
+        name=player.name,
+        host=player.host,
+        game_id=player.game_id,
+        birth_date=player.birth_date,
+        avatar=player.avatar,
+    )
     game = db.get(Game, new_player.game_id)
     if not game:
         raise HTTPException(status_code=404, detail="Game not found")
@@ -37,16 +53,18 @@ def create_player(player : Player_Base, db: Session = Depends(get_db)):
         db.add(new_player)
         try:
             db.commit()
-            db.refresh(new_player) #aca traigo el id generado por la db
+            db.refresh(new_player)  # aca traigo el id generado por la db
         except Exception as e:
             db.rollback()
-            raise HTTPException(status_code=400, detail=f"Error creating player: {str(e)}")
+            raise HTTPException(
+                status_code=400, detail=f"Error creating player: {str(e)}"
+            )
     return new_player
 
 
-@player.delete("/players/{player_id}", status_code=204,tags = ["Players"])
-def delete_player(player_id: int, db:Session = Depends(get_db)):
-    player = db.get(Player, player_id) 
+@player.delete("/players/{player_id}", status_code=204, tags=["Players"])
+def delete_player(player_id: int, db: Session = Depends(get_db)):
+    player = db.get(Player, player_id)
     if not player:
         raise HTTPException(status_code=404, detail="Player not found")
     try:
@@ -57,56 +75,75 @@ def delete_player(player_id: int, db:Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail=f"Error deleting player: {str(e)}")
     return None
 
-#el punto de esto es que al seleccionar un jugador le permitimos hacer acciones fuera de su turno, en eventos como en los cuales debe elegir un secretoa revelar y asi
 
-@player.put("/select/player/{player_id}",status_code = 201 ,tags = {"Players"})
-async def select_player (player_id : int, db : Session = Depends(get_db)) : 
+# el punto de esto es que al seleccionar un jugador le permitimos hacer acciones fuera de su turno, en eventos como en los cuales debe elegir un secretoa revelar y asi
+
+
+@player.put("/select/player/{player_id}", status_code=201, tags={"Players"})
+async def select_player(player_id: int, db: Session = Depends(get_db)):
     player = db.query(Player).filter(Player.player_id == player_id).first()
     if not player:
         raise HTTPException(status_code=404, detail="Player not found")
     game_id = player.game_id
-    player.isSelected = True 
-    try : 
+    player.isSelected = True
+    try:
         db.commit()
-        await broadcast_player_state (game_id)
-    except Exception as e : 
+        await broadcast_player_state(game_id)
+    except Exception as e:
         db.rollback()
         raise HTTPException(status_code=400, detail=f"Error selecting player: {str(e)}")
     return None
 
-@player.put("/unselect/player/{player_id}",status_code = 201 ,tags = {"Players"})
-async def unselect_player (player_id : int, db : Session = Depends(get_db)) : 
+
+@player.put("/unselect/player/{player_id}", status_code=201, tags={"Players"})
+async def unselect_player(player_id: int, db: Session = Depends(get_db)):
     player = db.query(Player).filter(Player.player_id == player_id).first()
     if not player:
         raise HTTPException(status_code=404, detail="Player not found")
     game_id = player.game_id
-    player.isSelected = False 
-    try : 
+    player.isSelected = False
+    try:
         db.commit()
-        await broadcast_player_state (game_id)
-    except Exception as e : 
+        await broadcast_player_state(game_id)
+    except Exception as e:
         db.rollback()
         raise HTTPException(status_code=400, detail=f"Error selecting player: {str(e)}")
     return None
 
-@player.put ("/vote/player/ {player_id}", status_code= 201 , tags = ["Players"])
-async def vote_player (player_id : int , db : Session = Depends (get_db)) : 
+
+@player.put("/vote/player/{player_id}", status_code=201, tags=["Players"])
+async def vote_player(player_id: int, db: Session = Depends(get_db)):
     player = db.query(Player).filter(Player.player_id == player_id).first()
     if not player:
         raise HTTPException(status_code=404, detail="Player not found")
     game_id = player.game_id
     player.votes_received += 1
-    game = db.query(Game).filter(Game.game_id == game_id).first() 
+    game = db.query(Game).filter(Game.game_id == game_id).first()
     if not game:
         raise HTTPException(status_code=404, detail="Game not found")
-    game.amount_votes += 1 
-    if game.amount_votes == game.players_amount : 
-        game.status = "in course"
+    game.amount_votes += 1
+    if game.amount_votes == game.players_amount:
+
+        winning_player = (
+            db.query(Player)
+            .filter(Player.game_id == game_id)
+            .order_by(desc(Player.votes_received))
+            .first()
+        )
+
+        if winning_player:
+
+            winning_player.isSelected = True
+
         game.amount_votes = 0
-    try : 
+        all_players = db.query(Player).filter(Player.game_id == game_id).all()
+        for p in all_players:
+            p.votes_received = 0
+
+    try:
         db.commit()
-        await broadcast_game_information (game_id)
-    except Exception as e : 
+        await broadcast_game_information(game_id)
+    except Exception as e:
         db.rollback()
         raise HTTPException(status_code=400, detail=f"Error selecting player: {str(e)}")
     return None
